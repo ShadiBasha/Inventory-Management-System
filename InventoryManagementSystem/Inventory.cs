21237ca76b49c9263c9f14e7fbd9628c6efaac62
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-10-31 08:46:31 +0200

    Inventory class: added a default constructor
    added a the default constructor to initialize the storage with a new
    list

--- InventoryManagementSystem/Inventory.cs
+++ InventoryManagementSystem/Inventory.cs
@@ -4,6 +4,11 @@ public class Inventory <T>
 {
     private List<T>? _storage;
 
+    public Inventory()
+    {
+        _storage = new List<T>();
+    }
+    
     public Inventory(List<T> storage)
     {
         _storage = storage;
@@ -13,5 +18,5 @@ public class Inventory <T>
     {
         _storage?.Add(item);
     }
-    
+
 }
