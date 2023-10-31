ca8d84f3761ddcc01f4fb6c6777c844ed2585657
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-10-31 11:24:32 +0200

    Program Class: added the Edit feature
    now the user can edit any of the products on the inventory

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -6,32 +6,78 @@ namespace InventoryManagementSystem;
 
 public class Program
 {
-    private static readonly Inventory<IStorable> Inventory = new();
-    
-    public static void  AddAProduct()
+    private static readonly Inventory Inventory = new();
+
+    public static void AddAProduct()
     {
-        Console.Clear();
         Console.WriteLine("Fill the needed data");
         Console.WriteLine("Product name : ");
         Console.ReadLine();
         string? name = Console.ReadLine();
+        Console.WriteLine("Product Price : ");
+        int price = 0;
+        price = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Product quantity : ");
         int quantity = 0;
         quantity = Convert.ToInt32(Console.ReadLine());
-        Product newProduct = new Product(name, quantity);
+        Product newProduct = new Product(name, price, quantity);
         Inventory.AddAnItem(newProduct);
-        Console.Clear();
         MainPage();
     }
 
     public static void ViewProducts()
     {
-        Console.Clear();
         Console.WriteLine("-------------------------------------\n");
         Console.WriteLine("Products in the inventory");
         Console.WriteLine(Inventory.ViewAllItems());
         Console.WriteLine("-------------------------------------");
+        Console.ReadLine();
         MainPage();
+    }
+
+    public static void EditAProduct()
+    {
+        Console.WriteLine("Please enter the name of the product");
+        Console.ReadLine();
+        string? name = Console.ReadLine();
+        Product productToEdit = Inventory.GetItemByName(name);
+        if (productToEdit.IsNull)
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Sorry there is no product with such name");
+            Console.WriteLine("----------------------------------------");
+            MainPage();
+            return;
+        }
+        Console.WriteLine("This is the product");
+        Console.WriteLine(productToEdit.ToString());
+        Console.WriteLine("What do you want to change?");
+        Console.WriteLine("1. The Name");
+        Console.WriteLine("2. The Price");
+        Console.WriteLine("3. The Quantity");
+        Console.WriteLine("Enter the number to continue");
+        Console.WriteLine("Enter anything else to exit");
+        int comand = Convert.ToInt32(Console.ReadLine());
+        switch (comand)
+        {
+            case 1:
+                Console.WriteLine("Enter the new Name for the product");
+                string newName = Console.ReadLine();
+                productToEdit.Name = newName;
+                break;
+            case 2:
+                Console.WriteLine("Enter the new Price for the product");
+                int price = Convert.ToInt32(Console.ReadLine());
+                productToEdit.Price = price;
+                break;
+            case 3:
+                Console.WriteLine("Enter the new Quantity for the product");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+                productToEdit.Quantity = quantity;
+                break;
+        }
+        MainPage();
+        return;
     }
 
     public static void MainPage()
@@ -47,6 +93,7 @@ public class Program
         Console.WriteLine("the command you want to do");
         Console.WriteLine("press anything else to exit");
         int comand = Console.Read();
+        Console.Clear();
         switch (comand)
         {
             case '1': 
@@ -56,6 +103,7 @@ public class Program
                 ViewProducts();
                 break;
             case '3': 
+                EditAProduct();
                 break;
             case '4':
                 break;
