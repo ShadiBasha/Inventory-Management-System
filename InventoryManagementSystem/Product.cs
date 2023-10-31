7c0ab0c28027ed915bf8c20c13ec9f4dc3c6a5e1
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-10-31 07:55:38 +0200

    Product Class: Constructor Editing
    Edited the default constructor to initialize the value of the Name and
    the Quantity and add the constructor that takes the name and the
    quantity from the user.

--- InventoryManagementSystem/Product.cs
+++ InventoryManagementSystem/Product.cs
@@ -3,11 +3,20 @@
 public class Product : IStorable
 {
     private static int _productNumber;
-    public string Name { get; set; } = "Product" + _productNumber;
-    public int Quantity { get; set; } = 0;
+    public string Name { get; set; }
+    public int Quantity { get; set; }
 
     public Product()
     {
+        Name = "Product" + _productNumber;
+        Quantity = 0;
+        _productNumber++;
+    }
+
+    public Product(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
         _productNumber++;
     }
 }
