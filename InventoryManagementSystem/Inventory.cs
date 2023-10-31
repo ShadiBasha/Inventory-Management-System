ffd6f9130291fc685123b237a46cf52852e76c3b
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-10-31 08:01:24 +0200

    Inventroy class creation
    The Inventory class is a generic class which contain a list of items,
    the default value of the storage (list of items) is null.
    added a constructor that takes a list of items and initialize the list
    with it.

--- InventoryManagementSystem/Inventory.cs
+++ InventoryManagementSystem/Inventory.cs
@@ -1,6 +1,12 @@
 ﻿namespace InventoryManagementSystem;
 
-public class Inventory
+public class Inventory <T>
 {
-    
+    private List<T>? _storage;
+
+    public Inventory(List<T> storage)
+    {
+        _storage = storage;
+    }
+
 }
