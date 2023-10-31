74b77fb657f5aa701b6e8c5218402aa34b869a7c
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-10-31 11:19:05 +0200

    Class Storable: deleted
    deleted the class because i didn't know how to handle the nulls
    looking forward to the null course

--- InventoryManagementSystem/Storable.cs
+++ InventoryManagementSystem/Storable.cs
@@ -1,7 +1,19 @@
-﻿namespace InventoryManagementSystem;
-
-public abstract class Storable
-{
-    public string Name { get; set; }
-    public int Quantity { get; set; }
-}
+﻿// using System.Data.SqlTypes;
+//
+// namespace InventoryManagementSystem;
+//
+// public class Storable : INullable
+// {
+//     public int Quantity { get; set; }
+//     public string Name { get; set; }
+//     public int Price { get; set; }
+//     public bool IsNull { get; set; }
+//
+//     public Storable()
+//     {
+//         IsNull = true;
+//         Price = 0;
+//         Quantity = 0;
+//         Name = "";
+//     }
+// }
